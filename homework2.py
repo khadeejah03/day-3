c646e3da5cdeb14d325967fb6c7b495379484c5e
Khadeejah03 <khadeejahosman037@gmail.com>
2025-08-08 12:07:47 +0100

    day3

---
+++ homework2.py
@@ -0,0 +1,9 @@
+#get input from user
+name = input ("enter name: ")
+acheivment = input ("enter achievment : ")
+
+#create congratulations message 
+ message = "congratulations" + khadeejah +"! you have achieved " + attending all your classes + "."
+
+# print the message 
+print (message )
